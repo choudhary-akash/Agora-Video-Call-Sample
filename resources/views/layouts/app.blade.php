99f16ca04f4f2af7d9ed2f9763cca86316d69499
Akash Choudhary <akashchoudhary1002@gmail.com>
2022-02-15 20:49:07 +0530

    Added Video Call Functionality Using Agora

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+	<script src="https://cdn.agora.io/sdk/release/AgoraRTCSDK-3.3.1.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('/agora-chat', 'App\Http\Controllers\AgoraVideoController@index');
+	Route::post('/agora/token', 'App\Http\Controllers\AgoraVideoController@token');
+	Route::post('/agora/call-user', 'App\Http\Controllers\AgoraVideoController@callUser');
+});
